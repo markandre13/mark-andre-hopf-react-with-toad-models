fac13b66b373e587d9c419b49f8e1a0a76fc7834
Mark-André Hopf <mhopf@mark13.org>
2021-05-03 21:30:30 +0200

    replace <Fragment> with <>

--- src/view/Food.tsx
+++ src/view/Food.tsx
@@ -13,11 +13,11 @@ export const Food: React.VFC<{survey: Survey}> = ({survey}) => {
       { title: "☕️ Breakfast", serving: survey.breakfast },
       { title: "🍱 Lunch", serving: survey.lunch },
       { title: "🍷 Dinner", serving: survey.dinner }].map(item =>
-        <React.Fragment>
+        <>
           <h3>{item.title}</h3>
           <Input label="Daily Servings" model={item.serving.numberOfMeals} />
           <Input label="Average Price €" model={item.serving.averagePrice} />
-        </React.Fragment>
+        </>
       )
     }
 
